11b2e9040acf6136da35d7769a8c834ba05b82c5
ZenWenGang <2822445966@qq.com>
2026-08-15 09:40:26 +0800

    Add files via upload

---
+++ HttpClient_8_13_1/widget.cpp
@@ -0,0 +1,41 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include<QNetworkReply>
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("客户端");
+    manager=new QNetworkAccessManager(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    //获取到输入框中的url
+    QUrl url(ui->lineEdit->text());
+    //构造一个HTTP请求对象
+    QNetworkRequest request(url);
+    //发送请求
+    QNetworkReply* response=manager->get(request);
+    //通过信号槽来处理响应
+    connect(response,&QNetworkReply::finished,this,[=](){
+        if(response->error()==QNetworkReply::NoError)
+            {
+            //响应正确获取到了
+            QString html=response->readAll();
+            ui->plainTextEdit->setPlainText(html);
+        }else//响应出错
+        {
+            ui->plainTextEdit->setPlainText(response->errorString());
+        }
+        //还要对respons进行释放
+        response->deleteLater();
+    });
+}
+
